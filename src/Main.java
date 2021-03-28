ae7d3162716183f49e30b9df274b903f23d6e7f5
VitalieVSV <dut66AHJCT>
2021-03-28 13:59:21 +0300

    Binary Function done

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,23 @@ import java.io.InputStreamReader;
 // 5 -> 101
 
 public class Main {
+
+    private static StringBuilder toBinary(int n) {
+        StringBuilder result = new StringBuilder();
+        int currNumber = n;
+
+        while (currNumber != 0) {
+            result.append(currNumber % 2);
+            currNumber /= 2;
+        }
+        return result.reverse();
+    }
+
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             int n = Integer.parseInt(br.readLine());
-          
+            System.out.print(toBinary(n));
         } catch (IOException e) {
             e.printStackTrace();
         }
