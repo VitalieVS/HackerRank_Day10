8fb53fb77a9c8b25ecbfa73b8ab63d54626ed12c
VitalieVSV <dut66AHJCT>
2021-03-28 13:54:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// 5 -> 101
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int n = Integer.parseInt(br.readLine());
+          
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
